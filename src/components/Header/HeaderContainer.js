24440fc15884d60abadc815cea93e28547766c21
NikitaT1 <nik.tati1@gmail.com>
2019-11-05 12:32:23 +0300

    added HeaderContainer

---
+++ src/components/Header/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Header.module.css';
+import Header from "./Header";
+
+class HeaderContainer extends React.Component {
+    render() {
+
+
+    return <Header/>
+
+    }
+}
+
+export default HeaderContainer;
